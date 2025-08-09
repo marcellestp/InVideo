1dab49c4260a87b90eae08bd3194bfccc805c289
Marcelle Pinheiro <marcellestp@gmail.com>
2025-08-08 19:53:14 -0700

    Fix_11, created db and tables, user session

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 """
 docstring here
 """
+import sqlite3
 import multiprocessing
 import os
 import time
-from flask import Flask, request, render_template, redirect, send_file
+from datetime import datetime
+from flask import Flask, request, render_template, redirect, send_file, session
 from invideo.tasks import *
+# from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 app = Flask(__name__)
-
+app.secret_key = 'your_super_secret_key_here'
 
 # Specify a directory to store uploaded files.
 UPLOAD_FOLDER = 'images/'
@@ -17,8 +21,178 @@ FILES_PATH = os.path.join(UPLOAD_FOLDER)
 PATH_VIDEO_TEMP = "static/out_temp.mp4"
 PATH_VIDEO = "static/output.mp4"
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
 
+user_id = 0
+database_file = "invideo.db"
+connection = sqlite3.connect(database_file, check_same_thread=False)
+db = connection.cursor()
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        user_name = request.form.get("username").lower()
+
+        # Query database for username
+        # db.execute(
+        #     "SELECT * FROM users WHERE username = ?", (request.form.get("username"),)
+        # )
+        db.execute(
+        "SELECT * FROM users WHERE username = ? OR email = ?",
+            (user_name, user_name)
+        )
+        rows = db.fetchall()
+        print(rows)
+
+        # Ensure username exist and password is correct
+        for row in rows:
+            if check_password_hash(
+                rows[0][3], request.form.get("password")):
+                # Remember which user has logged in
+                user_id = rows[0][0]
+                session['user_id'] = user_id
+            else:
+                return apology("invalid username and/or password", 400)
+
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")    
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    if request.method == "GET":
+        return render_template("register.html")
+
+    # User reached route via POST
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+        
+        # Ensure email was submitted
+        if not request.form.get("email"):
+            return apology("must provide email", 400)        
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        # Ensure password are same
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("different passwords", 400)
+
+        username = request.form.get("username").lower()
+        email = request.form.get("email").lower()
+        hash = request.form.get("password")
+
+        # Query database for username
+        db.execute(
+            "SELECT * FROM users WHERE email = ?", (email,)
+        )
+
+        rows = db.fetchall()
+
+        # Check if username already exist
+        if len(rows) != 0:
+            return apology("username already exist", 400)
+
+        # Query insert into database for username
+        db.execute(
+            "INSERT INTO users (username, email, hash, date) VALUES (?, ?, ?, ?)", (username
+                , email, generate_password_hash(hash), datetime.now())
+        )
+
+        connection.commit()
+        login()
+
+        # Redirect user to home page
+        return redirect("/")
+    
+
+@app.route("/changepass", methods=["GET", "POST"])
+# @login_required
+def changepass():
+    """Change password"""
+
+    # User reached route via POST (as by submitting a form via POST)
+    print("here")
+    if request.method == "GET":
+        
+        return render_template("changepass.html")
+
+    if request.method == "POST":
+        # Ensure email was submitted
+        if not request.form.get("email"):
+            return apology("must provide username", 400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        # Ensure password are same
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("different passwords", 400)
+
+        email = request.form.get("email").lower()
+
+        db.execute(
+        "SELECT * FROM users WHERE email = ?", (email,)
+        )
+        rows = db.fetchall()
+        # print(rows)
+        user_id = rows[0][0]
+
+        # Ensure user exist and password is correct
+        if rows:                        
+            # Query update password into database for username
+            db.execute(
+                "UPDATE users SET hash = ? WHERE id = ?", (generate_password_hash(
+                    request.form.get("password")), user_id)
+            )
+        else:
+            return apology("user not exist", 400)
+
+        # Redirect user to login
+        return redirect("/")
+    
+    
 @app.route('/download')
+@login_required
 def download_files():
     """
     It will download the video files in the user's Downloads directory.
@@ -30,6 +204,7 @@ def download_files():
 
 
 @app.route('/', methods=['GET', 'POST'])
+# @login_required
 def upload_process_download():
     """
     It will be responsible for identify which option the user
@@ -57,10 +232,25 @@ def upload_process_download():
     exist_video = check_video_exists()
     exist_file = check_file_upload_exists()
     file_list = check_quant_upload_exists()
-    
+
+    if 'user_id' in session:
+        user_id = session['user_id']
+    else:
+        user_id = 0
+
+    # print(user_id)
     return render_template('index.html', exist_video=exist_video,
         exist_file=exist_file, file_list=file_list)
 
+
+@app.route("/about", methods=["GET", "POST"])
+# @login_required
+def about():
+    """Get about app"""
+
+    if request.method == "GET":
+        return render_template("about.html")
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
